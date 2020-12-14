f2bd7718308700fd538cca22b8ba7fc36336f53e
Deivys Joenck <34344926+deivysjoenck@users.noreply.github.com>
2020-12-14 10:25:56 -0300

    Correção Tela Fundo - Reset

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,50 +1,83 @@
-@extends('layouts.padraoLogin')
+<!DOCTYPE html>
+<html>
+    <head>
 
-@section('content')
-<div class="container-fluid" style="margin-top: 23vh;">
+        <meta charset="UTF-8">
+        <title>{{ config('app.name') }} - Login</title> 
 
-    <div class="row justify-content-center">
+        <link rel='icon' href="{{ asset('images/favicon.png') }}">
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/padrao.css') }}">
+        <script src="{{ asset('js/app.js') }}" defer></script>
 
-        <div class="card border border-dark rounded" style="width: 23rem;">
-            <form method="POST" action="{{ route('password.update') }}">
-            @csrf
+        <style>
+            html, body {
 
-                <div class="card-header pl-5 pr-5">{{ __('Portal Financeiro - Cadastrar Senha') }}</div>
-                <div class="card-body pb-4 pl-5 pr-5">
+                padding-bottom: 0rem;
+                background-image: url("../../images/telafundo.png");
+                -webkit-background-size: cover;
+                -moz-background-size: cover;
+                -o-background-size: cover;
 
-                    <div class="form-row col-md-12">
+                background-size: cover;
+                background-repeat: no-repeat;
+                background-position: bottom;
+                background-attachment: fixed;
 
-                        <div class="col-md-12">
-                        <input type="hidden" name="token" value="{{ $token }}">
-                        {!! Form::label("email","E-mail" , ["class"=>"col-form-label pl-0"]) !!}
-                        {!! Form::email("email", "$email", ["class"=>"form-control", "readonly" ]) !!}
+            }
+        </style>   
+    </head>
+
+    <body>
+    <div id="app">
+
+        <div class="container-fluid" style="margin-top: 23vh;">
+        <div class="row justify-content-center">
+
+            <div class="card border border-dark rounded" style="width: 23rem;">
+                <form method="POST" action="{{ route('password.update') }}">
+                @csrf
+
+                    <div class="card-header pl-5 pr-5">{{ __('Portal Financeiro - Cadastrar Senha') }}</div>
+                    <div class="card-body pb-4 pl-5 pr-5">
+
+                        <div class="form-row col-md-12">
+
+                            <div class="col-md-12">
+                            <input type="hidden" name="token" value="{{ $token }}">
+                            {!! Form::label("email","E-mail" , ["class"=>"col-form-label pl-0"]) !!}
+                            {!! Form::email("email", "$email", ["class"=>"form-control", "readonly" ]) !!}
+                            </div>
+
+                            <div class="col-md-12">
+                            {!! Form::label("password", "Senha" , ["class"=>"col-form-label pl-0"]) !!}
+                            {!! Form::password("password", ["class"=>"form-control",  "autofocus" ]) !!}
+                            @if ($errors->has('password'))
+                                <span colspan='12' style="color: red;">
+                                    {{ $errors->first('password') }}
+                                </span>
+                            @endif
+                            </div>
+
+                            <div class="col-md-12">
+                            {!! Form::label("password-confirm", "Confirmação de Senha" , ["class"=>"col-form-label pl-0"]) !!}
+                            {!! Form::password("password-confirm", ["class"=>"form-control" ]) !!}
+                            </div>
+
                         </div>
-
-                        <div class="col-md-12">
-                        {!! Form::label("password", "Senha" , ["class"=>"col-form-label pl-0"]) !!}
-                        {!! Form::password("password", ["class"=>"form-control",  "autofocus" ]) !!}
-                        @if ($errors->has('password'))
-                            <span colspan='12' style="color: red;">
-                                {{ $errors->first('password') }}
-                            </span>
-                        @endif
-                        </div>
-
-                        <div class="col-md-12">
-                        {!! Form::label("password-confirm", "Confirmação de Senha" , ["class"=>"col-form-label pl-0"]) !!}
-                        {!! Form::password("password-confirm", ["class"=>"form-control" ]) !!}
-                        </div>
-
                     </div>
-                </div>
-                    
-                <div class="card-footer pl-5 pr-5">
-                    <div class="col-md-12">
-                    <button type="submit" class="btn btn-sm btn-secondary" style="width: 100px;">Cadastrar</button>
+                        
+                    <div class="card-footer pl-5 pr-5">
+                        <div class="col-md-12">
+                        <button type="submit" class="btn btn-sm btn-secondary" style="width: 100px;">Cadastrar</button>
+                        </div>
                     </div>
-                </div>
-            </form>
+                </form>
+            </div>
         </div>
     </div>
+    @include('layouts\footer')
+
 </div>
-@endsection
+</body>
+</html>
